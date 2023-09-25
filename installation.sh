5095ebb3f93ceba0923f0aaf6c02391d8a59ed7c
Mel-meel <mel.avizou@mail.ru>
2023-09-25 23:57:39 +0200

    Suite

--- installation.sh
+++ installation.sh
@@ -33,3 +33,20 @@ if [ "$distrib" = "*Ubuntu*" ] ; then
     paquets="tcl tk tcl8.6-tdbc tcl8.6-tdbc-mysql tcl8.6-tdbc-sqlite tcl8.6-tdbc-odbc"
     apt-get install -y $paquets
 fi
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        # ...
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+        # Mac OSX
+elif [[ "$OSTYPE" == "cygwin" ]]; then
+        # POSIX compatibility layer and Linux environment emulation for Windows
+elif [[ "$OSTYPE" == "msys" ]]; then
+        # Lightweight shell and GNU utilities compiled for Windows (part of MinGW)
+elif [[ "$OSTYPE" == "win32" ]]; then
+        # I'm not sure this can happen.
+elif [[ "$OSTYPE" == "freebsd"* ]]; then
+        # ...
+else
+        # Unknown.
+fi
+
